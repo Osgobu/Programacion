6f979a8d73d264bceaf882897d0a25a1242b195f
«Oscar» <oscargbuisa@gmail.com>
2023-12-15 11:52:45 +0100

    paso5

--- estroctures.py
+++ estroctures.py
@@ -1,16 +1,16 @@
 #PRE: Queremos saber cuales la informacion de cada estudiante y que notas tiene
 
-nom=input("Escriu el teu nom")
-Cognom=input("Escriu el teu cognom")
-edat=int(input("Escriu la teva edad"))
-nota1=int(input("Escriu la teva primera nota del teu examen"))
-nota2=int(input("Escriu la teva segona nota de l'examen"))
-nota3=int(input("Escriu la teva tercera nota"))
+nom=input("Escriu el teu nom:")
+cognom=input("Escriu el teu cognom:")
+edat=int(input("Escriu la teva edad:"))
+nota1=int(input("Escriu la teva primera nota del teu examen:"))
+nota2=int(input("Escriu la teva segona nota de l'examen:"))
+nota3=int(input("Escriu la teva tercera nota:"))
 
 alumno=(nom,cognom,edat,nota1,nota2,nota3)
 
 resultado_final= (nota1*0,30) + (nota2 * 0,40) + (nota3 * 0,30)
-if (resultado_final >= 7) or (nota1,nota2,nota3<=8):
+if (resultado_final <= 7) or nota1>=8 nota2>=8 nota3>=8:
     print(resultado_final)
 
 
