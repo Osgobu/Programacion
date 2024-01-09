f52dc16d6502b32a976fde3a88c47ee4ea26c8b1
«Oscar» <oscargbuisa@gmail.com>
2024-01-09 12:57:49 +0100

    añ speed

--- preguntes.py
+++ preguntes.py
@@ -1,2 +1,6 @@
-llistapreg(Como te llamas? )(Cuantos años tienes?)(Que signo eres? )(Que fecha es tu cumpleaños? )(Te gusta la pizza de piña? )
+llistapreg((Como te llamas? )(Cuantos años tienes?)(Que signo eres? )(Que fecha es tu cumpleaños? )(Te gusta la pizza de piña? ))
+
+seed = (seed * 997) % 1000
+random = (seed * 503) % 1000 / 100
+numero_pregunta = int(random * (len(llista_preguntes)))
 
