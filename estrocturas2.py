9438983662d82e4e154d4646f355fc2d502783f3
«Oscar» <oscargbuisa@gmail.com>
2024-01-12 12:09:08 +0100

    paso2

--- estrocturas2.py
+++ estrocturas2.py
@@ -1,3 +1,9 @@
-lista10=[]
-lista58=[]
 
+lista10=()
+lista58=()
+
+for i in range(10):
+    seed = (seed * 997) % 1000
+    random = (seed * 503) % 1000 / 100
+    lista10 = int(random * (len(llista_preguntes)))
+    
