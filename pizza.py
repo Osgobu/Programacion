36e796d7f4f66f3b6a4cc9e39f1b2997fe44996e
«Oscar» <oscargbuisa@gmail.com>
2024-02-01 11:51:09 +0100

    creamos la funcion

--- pizza.py
+++ pizza.py
@@ -1,6 +1,9 @@
-comanda=input("Escribe que pizza quieres: 1 pizza 4 quesos, 2 pizza de jamon con queso, 3 pizza carbonara, 4 pizza bbq ")
+comanda=int(input("Escribe que pizza quieres: 1 pizza 4 quesos, 2 pizza de jamon con queso, 3 pizza carbonara, 4 pizza bbq "))
 
 pizzaq=1
 pizzaj=2
 pizzac=3
 pizzab=4
+
+Comanda[]
+def Comanda(comanda):
