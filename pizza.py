5c6971af24ea605fa0564dfd82b5859bae3815f0
«Oscar» <oscargbuisa@gmail.com>
2024-02-01 11:57:07 +0100

    e quitado las variables no hacen falt soy burru

--- pizza.py
+++ pizza.py
@@ -1,9 +1,8 @@
 comanda=int(input("Escribe que pizza quieres: 1 pizza 4 quesos, 2 pizza de jamon con queso, 3 pizza carbonara, 4 pizza bbq "))
 
-pizzaq=1
-pizzaj=2
-pizzac=3
-pizzab=4
-
 Comanda[]
 def Comanda(comanda):
+    while comanda < 5:
+
+
+    
