b0e4a29516e0e14f15308ca58e5b4cd45d86cf6e
«Oscar» <oscargbuisa@gmail.com>
2024-02-01 12:20:43 +0100

    primera funcion hecha

--- pizza.py
+++ pizza.py
@@ -1,8 +1,25 @@
-comanda=int(input("Escribe que pizza quieres: 1 pizza 4 quesos, 2 pizza de jamon con queso, 3 pizza carbonara, 4 pizza bbq "))
 
-Comanda[]
-def Comanda(comanda):
+
+ccomanda=[]
+
+def Comanda(ccomanda):
+    comanda=0
     while comanda < 5:
+        comanda=int(input("Escribe que pizza quieres: 1 pizza 4 quesos, 2 pizza de jamon con queso, 3 pizza carbonara, 4 pizza Bbq "))
+        if comanda == 1:
+            print("Pizza 4 quesos")
+            ccomanda.append(comanda)
+        if comanda == 2:
+            print("Pizza jamon con queso")
+            ccomanda.append(comanda)
+        if comanda == 3:
+            print ("Pizza carbonara")
+            ccomanda.append(comanda)
+        if comanda == 4:
+            print("Pizza Bbq")  
+            ccomanda.append(comanda)
+Comanda(ccomanda)
 
 
-    
+
+
