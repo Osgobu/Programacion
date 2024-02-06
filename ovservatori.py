fe00dc09e1b14fb11ef676d4e92b2c46ea697d68
«Oscar» <oscargbuisa@gmail.com>
2024-02-06 12:39:06 +0100

    intento paso 2

--- ovservatori.py
+++ ovservatori.py
@@ -1,3 +1,10 @@
 print("Benvingut al registre de temperatures")
 print("-------------------------------------")
-
+print("[RT] Registre de temperatures setmanals.")
+print("[MJ] Consultar temperatura mitjana.")
+print("[[DF] Consultar diferència màxima.")
+print("[FI] Sortir. ")
+def temperatura():
+    temp=input("Escriu la tempreatura de dilluns a diumenge separades per ","")
+    temp.split(",")
+          
