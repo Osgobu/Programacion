37b5ccf01ded5764e18601e8b6c3aef8529f4433
«Oscar» <oscargbuisa@gmail.com>
2024-02-06 14:21:21 +0100

    intent de paso2

--- ovservatori.py
+++ ovservatori.py
@@ -6,9 +6,11 @@ print("[[DF] Consultar diferència màxima.")
 print("[FI] Sortir. ")
 
 #Esta funcion lo que hace es leer la temperaturas de la semana
-def rt():
+def RT():
     rt=int(input("Escribeme las temperatures de esta semana separado por ,"))
     rt.split(",")
  
-
-
+ #Esta funcion lo que hace es leer las temperaturas y hacer una mediana de ellas
+def MJ():
+    temperatura=int(input("Escribeme las primeras tempraturas ,"))
+    
