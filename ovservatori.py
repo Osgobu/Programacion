6468b3924dc60aa04b35cbc18a141f9fe43dd920
«Oscar» <oscargbuisa@gmail.com>
2024-02-06 13:59:09 +0100

    paso 1 modificado

--- ovservatori.py
+++ ovservatori.py
@@ -4,7 +4,11 @@ print("[RT] Registre de temperatures setmanals.")
 print("[MJ] Consultar temperatura mitjana.")
 print("[[DF] Consultar diferència màxima.")
 print("[FI] Sortir. ")
-def temperatura():
-    temp=input("Escriu la tempreatura de dilluns a diumenge separades per ","")
-    temp.split(",")
-          
+
+#Esta funcion lo que hace es leer la temperaturas de la semana
+def rt():
+    rt=int(input("Escribeme las temperatures de esta semana separado por ,"))
+    rt.split(",")
+ 
+
+
