3541e466f3ae177aa2ca2331d1c262e4b65caa23
«Oscar» <oscargbuisa@gmail.com>
2024-02-15 12:06:45 +0100

    creamos vaiables una para contar as y otraparaelfi

--- a.py
+++ a.py
@@ -1,9 +1,12 @@
+contador=0
+fi=False
+
 def mostrar():
     frase=input("Escribe una frase: ")
     print("Cuando quieras acabra has de scribir fi")
 #quiero que esta funcion,  escriba al usuario q tiene mas a que la frase anterior, y que te escriba la frase con mas a
 def masa ():
-    if frase 
+    if frase
 
 #Aqui qu te mueste el numero de a que tiene
     
