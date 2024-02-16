fc8d0409d3684b7b5281f28410070c3b0f7f874b
«Oscar» <oscargbuisa@gmail.com>
2024-02-16 11:43:59 +0100

    otro paso añadiendo fase 2

--- a.py
+++ a.py
@@ -2,11 +2,13 @@ contador=0
 fi=False
 
 def mostrar():
+    print("Este progrma sirve para comparar frases y ver cual tien mas A")
     frase=input("Escribe una frase: ")
-    print("Cuando quieras acabra has de scribir fi")
-#quiero que esta funcion,  escriba al usuario q tiene mas a que la frase anterior, y que te escriba la frase con mas a
-def masa ():
-    if frase
+    frase2=input("Escribe otra frase:")
+    print("Cuando quieras acabra has de escribir fi")
+#quiero que esta funcion compare las frases que ha escrito el cliente
+def comparar ():
+    
 
 #Aqui qu te mueste el numero de a que tiene
     
