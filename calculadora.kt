09167a64da0b510a79fd3eb946c1e9aacbc12041
«Oscar» <oscargbuisa@gmail.com>
2024-02-27 12:10:25 +0100

    division

--- calculadora.kt
+++ calculadora.kt
@@ -11,3 +11,10 @@ fun suma(){
     val resultado = numero1-numero2
     pinrtln("la suma de $numero1 y $numero2 es $resultado")
 }
+
+fun suma(){
+    val numero1 = 7
+    val numero2 = 3
+    val resultado = numero1/numero2
+    pinrtln("la suma de $numero1 y $numero2 es $resultado")
+}
