2253f7a39dbb05fb200c58ee677f01b04ffa221c
«Oscar» <oscargbuisa@gmail.com>
2024-02-27 12:30:25 +0100

    pensando como ser main

--- calculadora.kt
+++ calculadora.kt
@@ -5,23 +5,27 @@ fun suma(){
     pinrtln("la suma de $numero1 y $numero2 es $resultado")
 }
 
-fun suma(){
+fun restar(){
     val numero1 = 7
     val numero2 = 3
     val resultado = numero1-numero2
     pinrtln("la suma de $numero1 y $numero2 es $resultado")
 }
 
-fun suma(){
-    val numero1 = 7
-    val numero2 = 3
+fun dividir(){
+    val numero1 = 10
+    val numero2 = 2
     val resultado = numero1/numero2
     pinrtln("la suma de $numero1 y $numero2 es $resultado")
 }
 
-fun suma(){
+fun mulitplicar(){
     val numero1 = 7
     val numero2 = 3
     val resultado = numero1*numero2
     pinrtln("la suma de $numero1 y $numero2 es $resultado")
+}
+
+fun main(){
+    
 }
