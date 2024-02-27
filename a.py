977fcbc3d5bc1eeb9fecb0999640f0ed1606c0e3
«Oscar» <oscargbuisa@gmail.com>
2024-02-27 12:09:39 +0100

    la suma

--- a.py
+++ a.py
@@ -1,14 +1,18 @@
-contador=0
-fi=False
+#contador=0
+#fi=False
 
-def mostrar():
+def mostrar_frases():
     print("Este progrma sirve para comparar frases y ver cual tien mas A")
     frase=input("Escribe una frase: ")
     frase2=input("Escribe otra frase:")
     print("Cuando quieras acabra has de escribir fi")
 #quiero que esta funcion compare las frases que ha escrito el cliente
-def comparar ():
-    
+def comparar_frsaes ():
+    count1=0
 
-#Aqui qu te mueste el numero de a que tiene
-    
+
+#Aqui qu te mueste en pantalla el numero de a que tiene
+def mostrar_pantalla():
+
+
+#esta funcion quiero que haga que 
---
+++ calculadora.kt
@@ -0,0 +1,6 @@
+fun suma(){
+    val numero1 = 7
+    val numero2 = 3
+    val resultado = numero1+numero2
+    pinrtln("la suma de $numero1 y $numero2 es $resultado")
+}
