55553a6544c8e8845e1baf41ca619a6bc4a64768
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 11:21:18 +0200

    se que esta mal pero es mejor estp que nada asi que lo dejo como estaba poruqe no se me ocurre nada mejor

--- UF2/Sorting/Sorting.py
+++ UF2/Sorting/Sorting.py
@@ -5,4 +5,9 @@ class Sorting():
     def __init__(self):
         self sorting=Sorting()
     def llista_nombres(self) -> None:
-        
+        if nombres in self.stats.keys():
+            self.stats(nombres) 
+            print("El nombre es mes petit")
+        else:
+            self.stats(nombre) 
+            print("El nombre es major")
