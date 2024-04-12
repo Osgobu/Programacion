97216d86304867222b6bf9f818e011f63423998f
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 10:50:55 +0200

    creo la funcion no esta acabada

--- UF2/stats/stadisticas.py
+++
@@ -1,5 +0,0 @@
-#1.Crear una clase de Statistics
-#2.1.Crear una funcion estatica 
-
-class Statistics():
-
---
+++ UF2/stats/stats.py
@@ -0,0 +1,9 @@
+#1.Crear una clase de Statistics
+#2.1.Calcular la desviacion estandar
+
+class Statistics():
+    def desviacio(self):
+        
+    
+
+
