560908848399e550c40b74d981bee37a41989957
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 11:05:17 +0200

    importamos las estadisticas de nombres para poder saber ordenalos de mayor a menor

--- UF2/Sorting/Sorting.py
+++ UF2/Sorting/Sorting.py
@@ -1,8 +1,8 @@
 #1.Crear un clase llamada sorting
 #2.1 Crear una funcion que que ordene una lista 
-
+from stats import Statistics
 class Sorting():
     def __init__(self):
         self sorting=Sorting()
     def llista_nombres(self, nombre:str, numeroa:int) -> None:
-        
+        if nombre in self.
