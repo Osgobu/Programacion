c58aa4134ce7a25e1696159894f4a99b4a5639a5
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 11:02:36 +0200

    añado un valor a el nombre y al numeroa

--- UF2/Sorting/Sorting.py
+++ UF2/Sorting/Sorting.py
@@ -2,6 +2,7 @@
 #2.1 Crear una funcion que que ordene una lista 
 
 class Sorting():
-    def __init__(self)
-    def llista_nombres(self) -> None:
+    def __init__(self):
+        self sorting=Sorting()
+    def llista_nombres(self, nombre:str, numeroa:int) -> None:
         
