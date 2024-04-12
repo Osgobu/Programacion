45e89d9676242a0d3cbf554c0a574ba0d54b642b
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 11:32:41 +0200

    he creado la funcion y lo que tendria que hacer es mostrar al cliente el orden de la lista

--- UF2/main/main.py
+++ UF2/main/main.py
@@ -2,3 +2,8 @@
 #2.1 Mostrar en pantalla 
 #2.1.1.Crear una interficie clara y y ammigable con el usuario
 
+
+from Sorting import Sorting:
+
+def orden(self, lista:int)->None:
+print("Este es el orden de los numeros que usted a introducido")
--- UF2/stats/stats.py
+++ UF2/stats/stats.py
@@ -1,7 +1,9 @@
 #1.Crear una clase de Statistics
 #2.1.Calcular la desviacion estandar
 
+#Esta clase lo qeu tendria que hacer es tenr una funcion 
 class Statistics():
+    #Esta funcion tendria que hacer una desviacion estandar para ver los numeros que introduce el cliente
     def desviacio(self) -> None:
         
     
