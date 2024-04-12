f0fc16da689ed10825b896d45380d3797eab71d9
«Oscar» <oscargbuisa@gmail.com>
2024-04-12 10:55:27 +0200

    creamos el top dawn y la clase

---
+++ UF2/Sorting/Sorting.py
@@ -0,0 +1,5 @@
+#1.Crear un clase llamada sorting
+#2.1 Crear una funcion que que ordene una lista 
+
+class Sorting():
+
--- UF2/stas.py
+++
@@ -1,8 +0,0 @@
-#1.Crear una clase de Statistics
-#2.1crear una funcion estatica 
-#2.1.1
-
-
-
-#Creamos una clase para 
-class Statistics:
