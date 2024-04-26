74cfb5f27fbc80059a757374bc426defe8752c36
«Oscar» <oscargbuisa@gmail.com>
2024-04-26 10:43:38 +0200

    los atributos que e encontrado hasta ahora

--- UF3,4,5,6/PACMAN/Pacman.java
+++ UF3,4,5,6/PACMAN/Pacman.java
@@ -1,6 +1,11 @@
 package PACMAN;
 
 public class pacman;
-
     #buscar atributos 
+    private int puntuacion;
+    private int ancho;
+    private int altura;
+    private direccion;
+    private int vidas;
+    
 
