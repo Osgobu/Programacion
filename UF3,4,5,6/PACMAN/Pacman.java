44bdac2ad40984b575ea715421a4eeaad97b9e91
«Oscar» <oscargbuisa@gmail.com>
2024-04-26 10:52:14 +0200

    colocar pacman en una nueva posicion en el laberinto

--- UF3,4,5,6/PACMAN/Pacman.java
+++ UF3,4,5,6/PACMAN/Pacman.java
@@ -7,5 +7,17 @@ public class pacman;
     private int altura;
     private direccion;
     private int vidas;
-    
+    private int x;
+    private int y;
+
+    public void move();{
+        //Quiero comprobar si esta en la izquierda para empezar des de la derecha 
+        if (x<1 && y>310){
+            setx(1)
+            sety(310)
+        }
+
+    }
+
+
 
