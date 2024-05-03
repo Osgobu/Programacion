3225b5275a3c3d59e6108c6ba5fae43ccf6b518c
«Oscar» <oscargbuisa@gmail.com>
2024-05-03 12:04:21 +0200

    devolver valor de lo Nodos

--- UF3,4,5,6/ArbolBin/nodo1.java
+++ UF3,4,5,6/ArbolBin/nodo1.java
@@ -8,6 +8,21 @@ public class Nodo {
         this.izq = izq;
         this.der = der;
     }
-
-    
+ 
+ //Quiero que me devuelva los valores de la izquierda y de la derecha
+    public Nodo izq getIzq() {
+        return izq;
+    }
+ 
+    public void setIzq(Nodo izq) {
+        this.izq = izq;
+    }
+ 
+    public Nodo der getDer() {
+        return der;
+    }
+ 
+    public void setDer(Nodo der) {
+        this.der = der;
+    }
 }
