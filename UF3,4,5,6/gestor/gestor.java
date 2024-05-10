6b7566c2ff724785d2c5bfbb3ce969d2fd33d831
«Oscar» <oscargbuisa@gmail.com>
2024-05-10 11:01:53 +0200

    paso1

--- UF3,4,5,6/gestor/gestor.java
+++ UF3,4,5,6/gestor/gestor.java
@@ -1,11 +1,20 @@
 
-public class añadirTasca {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-    }
+class TaskManagement{
+    public TaskManagement(){}
+    public  void addTask(String taskName, String priority){}
+    public void doneTask(int taskId){}
+    public void deleteTask(int taskId){}
+    public void printAllTask(){}
+    public void printTaskByPriority(String priority){}
 }
-
-
-class marcar{
-    
+class main{
+    public void main(String[] args){
+        TaskManagement manager = new TaskManagement();
+        manager.addTask("Aprobar Programacio", "Alta")
+        manager.printAllTask();
+        manager.printTaskByPriority("Alta");
+        manager.doneTask(1);
+        manager.printAllTask();
+        manager.
+    }
 }
