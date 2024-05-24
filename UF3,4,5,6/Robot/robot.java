09503ca3884c48c28bdd3abd9dfb30df017c1b4f
«Oscar» <oscargbuisa@gmail.com>
2024-05-24 11:57:49 +0200

    5 Layout de 3*2

--- UF3,4,5,6/Robot/robot.java
+++ UF3,4,5,6/Robot/robot.java
@@ -3,7 +3,10 @@ import javax.swing.*
 
 //Esta clas ees para que muestre en pantalla una ventana del panel 
 public class RobotInterficie extends JFrame {
-    privte Robot robot;
+    privte Robot robot(){
+        
+    }
+
     //Constructor de la clase
     public RobotInterficie(){
         robot =new Robot("Robocop");
@@ -11,6 +14,13 @@ public class RobotInterficie extends JFrame {
         //Titulo como el del ejercicio en la ventana que se abrira y tamaño
         setTitle("Control de Robot");
         setSize(200,150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        //Estamoa haciendo que el layout de nuestro panel sea de 3*2
+        Jpanel panel = new JPanel();
+        pnael.setLayout(new GridLayout(3,2));
+
     }
     
 }
