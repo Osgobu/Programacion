892a796ffdc5767d96d21a75f9bf7a06916cabb5
«Oscar» <oscargbuisa@gmail.com>
2024-05-24 10:55:46 +0200

    3 hacer que el robot se gire y pueda ir hacia adelante atras y gire izquierda derecha

--- UF3,4,5,6/Robot/robot.java
+++ UF3,4,5,6/Robot/robot.java
@@ -2,7 +2,23 @@ public class robot{
     //nombre para identificar el robot le ponemos un atributo al robot
     private String nombre;
     //constructor 
-    public robot(String nombre)
-}
+    public robot(String nombre);
 
+    //Mover el robot hacia adelante
+    public void moveForward(){
+        System.out.println(nombre + "Se mueve hacia adelante");
+    }
+    //Mover el robot hacia atras
+    public void moveBackward(){
+        System.out.println(nombre + "Se mueve hacie atras");
+    }
+    //Mover el robot para que se gire hacia la izquierda
+    public void moveLeft(){
+        System.out.println(nombre + "Se esta girando a la izquierda");
+    }
+    //Mover el robot para que se gire hacia la derecha
+    public void moveRight(){
+        System.out.println(nombre + "Se esta girando a la derecha");
+    }
+}
 
