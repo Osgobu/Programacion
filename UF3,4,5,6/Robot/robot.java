11752a2bca01fb7355d1bd69d4a66c0086fab942
«Oscar» <oscargbuisa@gmail.com>
2024-05-24 11:44:39 +0200

    4 mostrar en una ventana emergente con un titulo y medida

--- UF3,4,5,6/Robot/robot.java
+++ UF3,4,5,6/Robot/robot.java
@@ -1,2 +1,16 @@
 import javax.swing.*
 
+
+//Esta clas ees para que muestre en pantalla una ventana del panel 
+public class RobotInterficie extends JFrame {
+    privte Robot robot;
+    //Constructor de la clase
+    public RobotInterficie(){
+        robot =new Robot("Robocop");
+
+        //Titulo como el del ejercicio en la ventana que se abrira y tamaño
+        setTitle("Control de Robot");
+        setSize(200,150);
+    }
+    
+}
