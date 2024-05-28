cbe36b65d37dbce06dddd26796a062db05d88548
«Oscar» <oscargbuisa@gmail.com>
2024-05-28 14:10:07 +0200

    crear el studen el databasemanage y un intento de ... por esta practica

---
+++ UF3,4,5,6/JavaDatabaseExample/src/main/java/com/example/database/DatabaseManager.java
@@ -0,0 +1,41 @@
+//package JavaBaseDatos.src.main.java.com.example.database;
+
+//import java.sql.Connection;
+//import JavaBaseDatos.src.main.java.com.example.database.models.Student;
+
+//public class DatabaseManager{
+//    public DatabaseManager(){
+//        try{
+//        }catch (SQLException e){
+
+//        }
+//    }
+//}
+
+//public void addStudent(){
+//        try{
+//        }catch (SQLException e){
+
+//        }
+//}
+
+//public void getStuddent(){
+//        try{
+//        }catch (SQLException e){
+
+//        }
+//}
+
+//public void updateStuddent(){
+//        try{
+//        }catch (SQLException e){
+
+//        }
+//}
+//
+//public void deleteStudent(){
+//        try{
+//        }catch (SQLException e){
+
+//        }
+//}
---
+++ UF3,4,5,6/JavaDatabaseExample/src/main/java/com/example/database/models/Student.java
@@ -0,0 +1,26 @@
+//package JavaBaseDatos.src.mian.java.com.example.database;
+
+//public class Student{
+ //   int id;
+//    private String nombre;
+//    private String apellido;
+
+//    public Student(int id, String nombre,String apellido){
+//        this.id=id;
+//        this.nombre=nombre;
+//        this.apellido=apellido;
+//    }
+//    public int getId(){
+//        return id;
+//    }
+//    public String getApellido(){
+ //       return apellido;
+//    }
+ //   public String setNombre(String nombre){
+//        this.nombre=nombre;
+  //  }
+//    public String setApellido(String apellido){
+  //      this.apellido=apellido;
+  //  } 
+//}
+
