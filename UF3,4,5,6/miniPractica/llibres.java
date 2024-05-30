7826d47a7492193ab7870a6ce69a981000d61317
«Oscar» <oscargbuisa@gmail.com>
2024-05-30 10:25:33 +0200

    1 haciendo un package con la clase libro y con el constructor

---
+++ UF3,4,5,6/miniPractica/llibres.java
@@ -0,0 +1,18 @@
+
+//clase del llibre en aquest package 
+public class Llibre{
+    private int id;
+    private String titol;
+
+    //constructor per el llibre
+    public Llibre(){}
+
+    public Llibre(int id, String titol){
+        this.id=id;
+        this.titol=titol
+    }
+
+
+
+}
+
