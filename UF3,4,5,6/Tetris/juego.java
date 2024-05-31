30a59f7a46e2f8e80a83b124be5b4d2941de2808
«Oscar» <oscargbuisa@gmail.com>
2024-05-31 12:07:06 +0200

    importtando swing en el package del juego y creacion de clase y de contruuctor

---
+++ UF3,4,5,6/Tetris/juego.java
@@ -0,0 +1,20 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+
+//creacion de clase y el contructor
+public class juego{
+    private int puntuacion;
+    private int nivel;
+    private boolean GameOver;
+    private Tablero tablero;
+    private InputHnadler InputHandler;
+
+    public juego(){
+        this.puntuacion=0;
+        this.nivel=1;
+        this.GameOver=false;
+        this.tablero=new Tablero(10,20)
+    }
+}
+
