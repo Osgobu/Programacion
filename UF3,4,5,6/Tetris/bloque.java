ccb6fac6c76fda7e0e7faa5917356b7910d412da
«Oscar» <oscargbuisa@gmail.com>
2024-05-31 12:14:27 +0200

    calse de bloque con el color y posicion

---
+++ UF3,4,5,6/Tetris/bloque.java
@@ -0,0 +1,20 @@
+//esta es la clase de el bloque en si con el color y la posicion, la posicion la hare mejor en la de piecas que es la que hare ahora
+
+public class bloque{
+    private Stirng color;
+    private int[] posicion;
+
+    public bloque(String color, int posicion){
+        this.color=color;
+        this.posicion=posicion;
+    }
+
+    public String getColor(){
+        return color:
+    }
+
+    public int[] getPosicion(){
+        return posicion
+    }
+
+}
